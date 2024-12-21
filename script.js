6e1ac5cb0a9d05970c3651ff7db58cab5e8b3d6c
khushboo <khushbookhushboo20093@gmail.com>
2024-12-21 10:26:52 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function moveFocus(currentInput, nextInputId) {
+    if (currentInput.value.length === 1) {
+        const nextInput = document.getElementById(nextInputId);
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
+
+function handleBackspace(event, currentInputId) {
+    if (event.key === "Backspace") {
+        const currentInput = document.getElementById(currentInputId);
+        if (currentInput.value === '') {
+            const previousInput = document.getElementById(getPreviousInputId(currentInputId));
+            if (previousInput) {
+                previousInput.focus();
+            }
+        }
+    }
+}
+
+function getPreviousInputId(currentInputId) {
+    const inputs = ['otp1', 'otp2', 'otp3', 'otp4', 'otp5', 'otp6'];
+    const currentIndex = inputs.indexOf(currentInputId);
+    return currentIndex > 0 ? inputs[currentIndex - 1] : null;
+}
